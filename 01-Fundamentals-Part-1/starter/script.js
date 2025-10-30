42ef7d66202c34c5361de336d1f882d21c42df7e
yzes95 <yehia_mnofeh_1995@hotmail.com>
2025-10-30 18:34:21 +0000

    updating the fundmentals_Part1 files with my test codes  and adding notes file

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,24 @@
+let j$s = "amazing";
+if (j$s === "amazing") console.log("JavaScript is FUN!");
+console.log(40 + 8 + 23 - 10);
+
+j$s = "taza"
+if (j$s === "amazing") console.log("JavaScript is FUN!")
+else console.log("JavaScript is not fun")
+
+const PI = 3.1415;
+console.log(PI);
+let country = "masr";
+let continent = "Africa";
+let population = 130000000;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+console.log(typeof j$s);
+console.log(typeof PI);
+console.log(typeof country);
+
+
+
+
